e3c7f7395d75e6664ecbc8072bf4f1b0347e139d
Gustavo Vieira Martins <gustavoafcavai@gmail.com>
2020-08-11 09:39:26 -0300

    Mobile: Style and route added on the page GiveClasses

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,9 +1,36 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View } from 'react-native';
+import { ImageBackground, Text, View } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import giveClassesBgImages from '../../assets/images/give-classes-background.png';
+import styles from './styles';
 
 function GiveClasses() {
+  const {goBack} = useNavigation();
+  
+  function handleNavigateBack() {
+    goBack();
+  }
+
   return (
-    <View/>
+    <View style={styles.container}>
+      <ImageBackground 
+        resizeMode="contain" 
+        source={giveClassesBgImages} 
+        style={styles.content}
+      >
+
+        <Text style={styles.title}>Que ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar na nossa plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton onPress={handleNavigateBack} style={styles.okBtn}>
+        <Text style={styles.okBtnText}>Tudo bem</Text>
+      </RectButton>
+
+    </View>
   );
 }
 
